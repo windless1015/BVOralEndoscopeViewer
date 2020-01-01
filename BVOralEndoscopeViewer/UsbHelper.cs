69768584de72f10071094bc94fb825e9e6dbc33d
windless1015 <shangtao1989@qq.com>
2020-01-01 14:20:42 +0800

    first commit

---
+++ BVOralEndoscopeViewer/UsbHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
+using LibUsbDotNet.DeviceNotify;
+
+namespace BVOralEndoscopeViewer
+{
+    class UsbHelper
+    {
+        private UsbDevice MyUsbDevice;
+        private IDeviceNotifier UsbDeviceNotifier = DeviceNotifier.OpenDeviceNotifier();
+        private UsbEndpointWriter writer;
+        private UsbEndpointReader reader;
+
+        public bool OpenDevice(int vid, int pid)
+        {
+            UsbDeviceFinder myUsbFinder = new UsbDeviceFinder(vid, pid);
+            UsbRegDeviceList DeviceList = UsbDevice.AllLibUsbDevices;
+            UsbRegistry RegInfo = DeviceList.Find(myUsbFinder);
+            if (RegInfo != null)
+            {
+                MyUsbDevice = UsbDevice.OpenUsbDevice(myUsbFinder);
+            }
+            return true;
+             
+        }
+    }
+}
